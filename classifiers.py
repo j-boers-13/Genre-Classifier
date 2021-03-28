333f004c94684cbb0228cf31b7db89763b870c16
j-boers-13 <jeroen.boers1@gmail.com>
2021-03-28 22:43:08 +0200

    Updated preprocessing

--- classifiers.py
+++ classifiers.py
@@ -33,6 +33,8 @@ def split_train_test(feature_samples, split=0.8):
     return train_feats, test_feats
 
 def main():
+    pd.set_option('display.max_columns', None)
+
     df_genre = pd.read_csv('data/title.basics.tsv',sep='\t')
     df_genre = df_genre[df_genre['titleType'] == 'movie']
     df_genre = df_genre[df_genre['genres'] != '\\N']
@@ -43,16 +45,33 @@ def main():
 
     # Random sample half of the data
     df_merged = df_merged_full.sample(frac=0.5)
-    # Split genre string into array
-    df_merged['genres'] = df_merged['genres'].apply(lambda x: x.split(','))
-    # Create a row per genre
-    df_merged = df_merged.explode('genres').fillna('')
-    # Preview data to see if 1 genre per row
-    print(df_merged.head(5))
 
+    # Split genres, writers and directors strings into array
+    df_merged['genres'] = df_merged['genres'].apply(lambda x: x.split(','))
+    df_merged['writers'] = df_merged['writers'].apply(lambda x: x.split(','))
+    df_merged['directors'] = df_merged['directors'].apply(lambda x: x.split(','))
+
+    # Create directors df
+    df_directors = pd.DataFrame([pd.Series(x) for x in df_merged.directors])
+    df_directors.columns = ['director_{}'.format(x+1) for x in df_directors.columns]
+
+    # Create writers df
+    df_writers = pd.DataFrame([pd.Series(x) for x in df_merged.writers])
+    df_writers.columns = ['writer_{}'.format(x+1) for x in df_writers.columns]
+
+    # Create a duplicate row per genre
+    df_merged = df_merged.explode('genres').fillna('')
+    df_merged = df_merged.reset_index()
+    df_directors = df_directors.reset_index()
+    df_writers = df_writers.reset_index()
+    df_merged = pd.concat([df_merged, df_directors], axis=1)
+    df_merged = pd.concat([df_merged, df_writers], axis=1)
+    
     # Get one hot encoded sparse-matrix
     one_hot_encoder = OneHotEncoder(categories='auto')
-    feature_matrix = one_hot_encoder.fit_transform(df_merged[['writers','directors']])
+    column_names = [col for col in df_merged.columns if 'director' in col].extend([col for col in df_merged.columns if 'writer' in col])
+
+    feature_matrix = one_hot_encoder.fit_transform(df_merged[column_names])
     
     # Get list of genres per observation
     labels = df_merged['genres'].tolist()    
