61b4b52d6bc83a88f7c9df50376ec1e3c9afba29
j-boers-13 <jeroen.boers1@gmail.com>
2021-03-28 15:02:36 +0200

    Fixed code

--- classifiers.py
+++ classifiers.py
@@ -1,6 +1,5 @@
 from sklearn import svm
 import pandas as pd
-from constants import TITLE_BASICS, TITLE_CREW
 
 # Train an svm classifier
 # Hier kunnen we de SVM soort veranderen in:
@@ -14,13 +13,13 @@ def get_features_labels(data):
     return
 
 def main():
-    df_genre = pd.read_csv(TITLE_BASICS,sep='\t')
-    df_genre2 = df_genre[df_genre.titleType == 'movie']
-    df_genre2 = df_genre2[df_genre.genres != '\\N']
+    df_genre = pd.read_csv('data/title.basics.tsv',sep='\t')
+    df_genre = df_genre[df_genre['titleType'] == 'movie']
+    df_genre = df_genre[df_genre['genres'] != '\\N']
     #print(df_genre2)
-    df_crew = pd.read_csv('title.crew.tsv',sep='\t')
+    df_crew = pd.read_csv('data/title.crew.tsv',sep='\t')
     #print(df_crew)
-    df_merged = pd.merge(left=df_genre2, right=df_crew, left_on='tconst', right_on='tconst')
+    df_merged = pd.merge(left=df_genre, right=df_crew, left_on='tconst', right_on='tconst')
     print(df_merged)
     return
 
