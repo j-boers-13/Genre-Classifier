d70dc2acbd78d33d4f17bf407bd245a3c789b952
j-boers-13 <jeroen.boers1@gmail.com>
2021-03-29 12:48:41 +0200

    pd duimmies classifier

--- classifiers.py
+++ classifiers.py
@@ -38,46 +38,59 @@ def main():
     df_genre = pd.read_csv('data/title.basics.tsv',sep='\t')
     df_genre = df_genre[df_genre['titleType'] == 'movie']
     df_genre = df_genre[df_genre['genres'] != '\\N']
-    print("reading crew")
     df_crew = pd.read_csv('data/title.crew.tsv',sep='\t')
 
     df_merged_full = pd.merge(left=df_genre, right=df_crew, left_on='tconst', right_on='tconst')
 
     # Random sample half of the data
-    df_merged = df_merged_full.sample(frac=0.5)
+    df_merged = df_merged_full.sample(n=10000)
 
     # Split genres, writers and directors strings into array
+    # These strings are formatted like genre1,genre2,genre3 etc.
     df_merged['genres'] = df_merged['genres'].apply(lambda x: x.split(','))
-    df_merged['writers'] = df_merged['writers'].apply(lambda x: x.split(','))
-    df_merged['directors'] = df_merged['directors'].apply(lambda x: x.split(','))
+    # df_merged['writers'] = df_merged['writers'].apply(lambda x: x.split(','))
+    # df_merged['directors'] = df_merged['directors'].apply(lambda x: x.split(','))
 
-    # Create directors df
-    df_directors = pd.DataFrame([pd.Series(x) for x in df_merged.directors])
-    df_directors.columns = ['director_{}'.format(x+1) for x in df_directors.columns]
+    # # Create split directors df
+    # df_directors = pd.DataFrame([pd.Series(x) for x in df_merged.directors])
+    # df_directors.columns = ['director_{}'.format(x+1) for x in df_directors.columns]
 
-    # Create writers df
-    df_writers = pd.DataFrame([pd.Series(x) for x in df_merged.writers])
-    df_writers.columns = ['writer_{}'.format(x+1) for x in df_writers.columns]
+    # # Create split writers df
+    # df_writers = pd.DataFrame([pd.Series(x) for x in df_merged.writers])
+    # df_writers.columns = ['writer_{}'.format(x+1) for x in df_writers.columns]
 
-    # Create a duplicate row per genre
+    # Create a duplicate row per genre (split genres)
     df_merged = df_merged.explode('genres').fillna('')
-    df_merged = df_merged.reset_index()
-    df_directors = df_directors.reset_index()
-    df_writers = df_writers.reset_index()
-    df_merged = pd.concat([df_merged, df_directors], axis=1)
-    df_merged = pd.concat([df_merged, df_writers], axis=1)
-    
-    # Get one hot encoded sparse-matrix
-    one_hot_encoder = OneHotEncoder(categories='auto')
-    column_names = [col for col in df_merged.columns if 'director' in col].extend([col for col in df_merged.columns if 'writer' in col])
 
-    feature_matrix = one_hot_encoder.fit_transform(df_merged[column_names])
+    # # Reset indices for joining
+    # df_merged = df_merged.reset_index()
+    # df_directors = df_directors.reset_index()
+    # df_writers = df_writers.reset_index()
+
+    # # Join the dataframes
+    # df_merged = pd.concat([df_merged, df_directors], axis=1)
+    # df_merged = pd.concat([df_merged, df_writers], axis=1)
+
+    # Replace \N string by np.nan (moet dit None zijn?)
+    df_merged.replace("\\N", np.nan)
     
+    column_names = df_merged.filter(regex='writer|director').columns.tolist()
+    column_names = [name for name in column_names if name not in ['writers', 'directors']]
+
+    # Convert columns dtype from objects to strings
+    # For memory optimization
+    df_merged[column_names] = df_merged[column_names].astype(str)
+
+    # Get one hot encoded sparse-matrix
+    # Deze kan beter dealen met NA values, ik heb nu \N ook NA gemaakt,
+    # dus kan die alles handlen
+    feature_matrix = pd.get_dummies(df_merged[['writers','directors']], dummy_na=True)
+
     # Get list of genres per observation
-    labels = df_merged['genres'].tolist()    
+    labels = df_merged['genres'].tolist()
 
     # Train SVM model
-    train_svm(feature_matrix, labels)
+    classifier = train_svm(feature_matrix, labels)
 
 
 if __name__ == "__main__":
